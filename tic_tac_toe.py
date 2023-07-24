45816f87c8d475b8ab173dbcfe1e1bc2cbf19e81
sh-0-go <shogo1216.kiki@gmail.com>
2023-07-24 17:48:54 +0900

    〇と×を表示

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,6 +8,18 @@ def drow_grid():
         pygame.draw.line(screen, BLACK, (0, i * 200), (screen_width, i * 200), 5)
         pygame.draw.line(screen, BLACK, (i * 200, 0), (i * 200, screen_height), 5)
 
+# ボードの描画
+def drow_board():
+    for row_index, row in enumerate(board):
+        for col_index, col in enumerate(row):
+            if col == 1:
+                # 〇を描画
+                pygame.draw.circle(screen, RED, (col_index * 200 + 100, row_index * 200 + 100), 80, 5)
+            elif col == -1:
+                # ×を描画
+                pygame.draw.line(screen, BLUE, (col_index * 200 + 20, row_index * 200 + 20), (col_index * 200 + 180, row_index * 200 + 180), 5)
+                pygame.draw.line(screen, BLUE, (col_index * 200 + 180, row_index * 200 + 20), (col_index * 200 + 20, row_index * 200 + 180), 5)
+
 # ウィンドウの作成
 screen_width = 600
 screen_height = 600
@@ -23,14 +35,9 @@ BLUE = (0, 0, 255)
 
 # ボード(0: 空白，1: 〇，-1: ×)
 board = [
-    [1, 0, 0],
-    [0, -1, 0],
-    [0, 0, 1]]
-
-for row_index, row in enumerate(board):
-    for col_index, col in enumerate(row):
-        if col == 1:
-            print(col_index, col)
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]]
 
 # メインループ#####################################################
 run = True
@@ -41,6 +48,9 @@ while run:
     
     # グリッド線の描画
     drow_grid()
+
+    # ボードの描画
+    drow_board()
 
     # イベントの取得
     for event in pygame.event.get():
